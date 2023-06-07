2d2b8b132b5ca918ca4ce831f69410a2aede56ee
Thomás Kuhn <thomas.kuhn@thomask-ltm4wrw.internal.salesforce.com>
2023-06-07 16:20:46 -0700

    First movie with rating added

--- script.js
+++ script.js
@@ -76,6 +76,38 @@ let fakeData =
 }
 
 
-let firstMovie = fakedata.results[0]
+let firstMovie = fakeData.results[0]
 
 console.log(firstMovie)
+
+function generateCards(movieObject) {
+
+    //create star
+    let star = document.createElement('span');
+    star.classList.add('star')
+    let starContent = document.createTextNode('⭐️');
+    star.appendChild(starContent);
+    
+
+    //create rating
+    let rating = document.createElement('span');
+    let ratingContent = document.createTextNode(movieObject.vote_average);
+    rating.classList.add('rating')
+    rating.appendChild(ratingContent);
+    
+
+    //create average container
+    let averageContainer = document.createElement('div');
+    averageContainer.classList.add('average')
+    averageContainer.appendChild(star);
+    averageContainer.appendChild(rating)
+    document.body.appendChild(averageContainer)
+
+    let image = document.createElement('img');
+    image.src = "https://image.tmdb.org/t/p/w342" + movieObject.poster_path
+    document.body.insertBefore(image, averageContainer)
+
+
+}
+
+generateCards(firstMovie);
