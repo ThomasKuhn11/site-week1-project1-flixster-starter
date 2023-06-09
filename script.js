f3762d3fea0f9dc0671430ecaea22e43aa5013c5
Thomás Kuhn <thomas.kuhn@thomask-ltm4wrw.internal.salesforce.com>
2023-06-08 21:04:49 -0700

    load more working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let apiToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDExYzRmMjJkN2VmMzRhNWU1MmQwMGNlZjgzZDBlMSIsInN1YiI6IjY0ODIwNDg5YmYzMWYyMDExZDQyMTE0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qDBwCjX6tmtAwMEnJQ5J-5-jMP5JW-9reCkDqiD-n-s";
 
-const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1'
+const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1'  //notice that last letter is the page
 
 const options = {
     method: 'GET',
@@ -11,29 +11,22 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDExYzRmMjJkN2VmMzRhNWU1MmQwMGNlZjgzZDBlMSIsInN1YiI6IjY0ODIwNDg5YmYzMWYyMDExZDQyMTE0YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qDBwCjX6tmtAwMEnJQ5J-5-jMP5JW-9reCkDqiD-n-s'
     }
   };
-  
 
-//eventHandler for when load more is clicked
+
+
+
 
 //api have pages
 
-let currentPage = 0
 
+  let currentPage = 1
 
-// async function logJSONData() {
-//     const response = await fetch(url, options);
-//     const jsonData = await response.json();
-//     console.log(jsonData);
+  getMovies(currentPage)
+  
 
-//     for (let i = 0; i < data.results.length; i++ ) {
-//         generateCards(data.results[i]);
-            
-//     }
-// }
-
-
-//function getMovies() {
-    fetch(url, options)
+function getMovies(pageNum) {
+    dinamicURL = url.slice(0, -1) + pageNum;
+    fetch(dinamicURL, options)
     .then(response => {return response.json()})
     .then(response => {
     console.log(response)
@@ -41,15 +34,30 @@ let currentPage = 0
     })
     .then(data => {
     for (let i = 0; i < data.results.length; i++ ) {
-        generateCards(data.results[i]);
-    
+        generateCards(data.results[i]);   
     }
+    //alert('hi')
+
 }
 )
+}
 
 
-//}
+//eventHandler for when load more is clicked
+//create event lisener so that every click we call we will call getMovies again for the next page
 
+  
+  const button = document.getElementById('loadButton');
+  
+
+  button.addEventListener('click', function() {
+    getMovies(++currentPage);
+  });
+
+
+
+
+//create apiPage variable to keep track of page
 
 
 
@@ -93,7 +101,10 @@ function generateCards(movieObject) {
     movie.appendChild(image);
     movie.appendChild(averageContainer);
     movie.appendChild(name);
-    document.body.appendChild(movie);
+
+
+    const mainContainer = document.getElementById("movies")
+    mainContainer.appendChild(movie);
 
 
     movie.style.display = 'inline-block';
@@ -102,118 +113,8 @@ function generateCards(movieObject) {
     movie.style.boxSizing = 'border-box';
     movie.style.textAlign = 'center';
 
-    movie.style.backgroundColor = 'grey';
 
     movie.style.marginLeft = '45px'; // Adjust the margin value as needed
 
-
-
-
 }
 
-
-
-
-
-
-
-
-
-/*
-
-let fakeData = 
-{
-    "dates": {
-        "maximum": "2023-06-05",
-        "minimum": "2023-04-18"
-    },
-    "page": 1,
-    "results": [
-        {
-            "adult": false,
-            "backdrop_path": "/9n2tJBplPbgR2ca05hS5CKXwP2c.jpg",
-            "genre_ids": [
-                16,
-                10751,
-                12,
-                14,
-                35
-            ],
-            "id": 502356,
-            "original_language": "en",
-            "original_title": "The Super Mario Bros. Movie",
-            "overview": "While working underground to fix a water main, Brooklyn plumbers—and brothers—Mario and Luigi are transported down a mysterious pipe and wander into a magical new world. But when the brothers are separated, Mario embarks on an epic quest to find Luigi.",
-            "popularity": 3392.2,
-            "poster_path": "/qNBAXBIQlnOThrVvA6mA2B5ggV6.jpg",
-            "release_date": "2023-04-05",
-            "title": "The Super Mario Bros. Movie",
-            "video": false,
-            "vote_average": 7.8,
-            "vote_count": 4327
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/2I5eBh98Q4aPq8WdQrHdTC8ARhY.jpg",
-            "genre_ids": [
-                28,
-                12,
-                16,
-                878
-            ],
-            "id": 569094,
-            "original_language": "en",
-            "original_title": "Spider-Man: Across the Spider-Verse",
-            "overview": "After reuniting with Gwen Stacy, Brooklyn’s full-time, friendly neighborhood Spider-Man is catapulted across the Multiverse, where he encounters the Spider Society, a team of Spider-People charged with protecting the Multiverse’s very existence. But when the heroes clash on how to handle a new threat, Miles finds himself pitted against the other Spiders and must set out on his own to save those he loves most.",
-            "popularity": 2921.844,
-            "poster_path": "/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg",
-            "release_date": "2023-05-31",
-            "title": "Spider-Man: Across the Spider-Verse",
-            "video": false,
-            "vote_average": 8.8,
-            "vote_count": 739
-        },
-        {
-            "adult": false,
-            "backdrop_path": "/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg",
-            "genre_ids": [
-                28,
-                80,
-                53
-            ],
-            "id": 385687,
-            "original_language": "en",
-            "original_title": "Fast X",
-            "overview": "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
-            "popularity": 2334.66,
-            "poster_path": "/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
-            "release_date": "2023-05-17",
-            "title": "Fast X",
-            "video": false,
-            "vote_average": 7.1,
-            "vote_count": 854
-        },
-    ],
-    "total_pages": 98,
-    "total_results": 1951
-}
-
-*/
-
-/*
-let firstMovie = fakeData.results[0]
-
-console.log(firstMovie)
-*/
-
-
-//generateCards(firstMovie);
-
-
-/*
-for (let i = 0; i < 3; i++ ) {
-    generateCards(fakeData.results[i]);
-
-}
-
-*/
-//create for loop to add each movie using logic above
